1c5c622b7212073bc0810f9df093320dc3a595ef
Leo7735 <104826766+Leo7735@users.noreply.github.com>
2022-05-23 17:04:59 +0800

    Update Main.java

--- Chess0/src/Main.java
+++ Chess0/src/Main.java
@@ -8,7 +8,7 @@ public class Main  {
     public static void main(String[] args) {
        SwingUtilities.invokeLater(() -> {
             User_Login logFrame=new User_Login(350,200,"白方选手登录：");
-           
+            logFrame.setVisible(true);
         });
     }
 }
