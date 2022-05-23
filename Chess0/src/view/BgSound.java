ea1e58bc890c10488c384b6b51b7e813b116e5de
Leo7735 <104826766+Leo7735@users.noreply.github.com>
2022-05-23 17:06:59 +0800

    Update BgSound.java

--- Chess0/src/view/BgSound.java
+++ Chess0/src/view/BgSound.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,14 +9,15 @@ public class BgSound {
         private static Clip bgm;//背景乐
         private static Clip hit;//音效
         private static AudioInputStream ais;
-        private int WavIndex=1;
-        BgSound(){}
-
+        private int WavIndex=1;     //背景音乐序号，最大到5
+        //背景音乐播放
         public void play(){
             try {
                 bgm=AudioSystem.getClip();
-                String fname="sounds/bgmusic/bg"+WavIndex+".wav";
-                InputStream is=BgSound.class.getClassLoader().getResourceAsStream(fname);
+                String filename="sounds/music/bg"+WavIndex+".wav";
+
+                InputStream is_0=getClass().getClassLoader().getResourceAsStream(filename);
+                BufferedInputStream is = new BufferedInputStream(is_0);
                 //getclassLoader得到当前类的加载器.getResourceAsStream加载资源，只能加载wav的音乐格式
                 if (is != null) {
                     ais=AudioSystem.getAudioInputStream(is);//获取输入流
@@ -27,11 +29,13 @@ public class BgSound {
             bgm.start();//开始播放
             bgm.loop(Clip.LOOP_CONTINUOUSLY);//循环播放
         }
+        //声音效果播放，需要送入约定声音文件名 如按钮音"Button" 移动音"Move" 击杀音"Kill"
         public void play(String name){
             try {
                 hit=AudioSystem.getClip();
-                String fname="sounds/acsound/"+name+".wav";
-                InputStream is=BgSound.class.getClassLoader().getResourceAsStream(fname);
+                String filename="sounds/sound/"+name+".wav";
+                InputStream is_0=getClass().getClassLoader().getResourceAsStream(filename);
+                BufferedInputStream is = new BufferedInputStream(is_0);
                 //getclassLoader得到当前类的加载器.getResourceAsStream加载资源，只能加载wav的音乐格式
                 if (is != null) {
                     ais=AudioSystem.getAudioInputStream(is);//获取输入流
@@ -42,6 +46,7 @@ public class BgSound {
             }
             hit.start();//开始播放
         }
+        //停止背景音乐播放
         public void stop()
         {
             if(ais!=null) {
@@ -53,3 +58,4 @@ public class BgSound {
             }
         }
 }
+
